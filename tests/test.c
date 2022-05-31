efe25b6b3034391cb821a47be0264b098b2db34a
theob34 <theo.bos@hotmail.fr>
2022-05-31 17:21:53 +0200

    Création du programme a tester

--- tests/test.c
+++ tests/test.c
@@ -1,8 +1,16 @@
 int main(){
-    int a = 2;
-    int b = 3;
-    int c = a + b;
-    printf(c);
-
-    return c;
+    int a = 3;
+    a = 4;
+    if (a == 4) {
+        int b = 5;
+    }
+    else {
+        int b = 6;
+    }
+    int i = 0;
+    while (i < 3) {
+        i = i + 1 ;
+    }
+    i = 3 * a + 4 / a - i;
+    return 0;
 }
